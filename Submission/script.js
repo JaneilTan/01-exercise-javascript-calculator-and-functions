f32795f8a5ea3d29cf29c6aed0c9814ebc11ef91
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-03-08 18:46:52 +1300

    Create multiply function

--- Submission/script.js
+++ Submission/script.js
@@ -7,6 +7,10 @@ const add = (num1, num2) => {
 const subtract = (num1, num2) => {
   return num1 - num2;
 }
+const multiply = (num1, num2) => {
+  return num1 * num2;
+}
+
 /**
  * PART A: Convert all the following functions to use arrow functions
  * Note: The following calculator is taken from this tutorial:
